105c14f44ae3f2d5bf138e1775e2f05f3651585f
WenDEVLIFE <frouenmedinajr@gmail.com>
2026-05-04 00:46:35 +0800

    refactor: exclude Web directory from project build and source compilation

--- Mental_web/obj/Debug/net10.0-windows/Mental_web.AssemblyInfo.cs
+++ Mental_web/obj/Debug/net10.0-windows/Mental_web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mental_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c33cf0bcad3842d948149ebcaf6bb5ab7797f784")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6506515f0239c185fca2aa0240bfbc327fc0f305")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mental_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mental_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
